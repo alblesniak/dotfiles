2e4da1ee75568ccec09fd391ce77b2eb19e3e964
alblesniak <alblesniak@users.noreply.github.com>
2022-08-11 16:22:39 +0200

    Redirect standard output to black hole

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,7 +4,7 @@ echo '\n<<< Starting ZSH Setup >>>\n'
 
 # Change shell with root permissions
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo "/usr/local/bin/zsh" | sudo tee -a "/etc/shells"
+echo "/usr/local/bin/zsh" | sudo tee -a "/etc/shells" >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s "/usr/local/bin/zsh"
