c88fa82df3974525c902bd63f976a367c3d90df8
alblesniak <alblesniak@users.noreply.github.com>
2022-08-25 14:26:51 +0200

    setup for macos added

--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -74,11 +74,6 @@ _G.packer_plugins = {
     path = "/Users/alb/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
-  ["barbar.nvim"] = {
-    loaded = true,
-    path = "/Users/alb/.local/share/nvim/site/pack/packer/start/barbar.nvim",
-    url = "https://github.com/romgrk/barbar.nvim"
-  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/Users/alb/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
---
+++ setup_macos.zsh
@@ -0,0 +1,52 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting macOS Setup >>>\n"
+
+### DOCK ###
+
+# Set position of Dock to the left
+defaults write com.apple.dock "orientation" -string "left"
+
+# Set the icon size of Dock items to 35 pixels
+defaults write com.apple.Dock "tilesize" -int 35
+
+# Set the dock to autohide
+defaults write com.apple.Dock "autohide" -bool "true" 
+
+# Set the dock autohide delay to 0
+defaults write com.apple.Dock "autohide-delay" -float 0
+
+# Set the dock autohide animation to 0
+defaults write com.apple.Dock "autohide-time-modifier" -float 0
+
+# Sety to not show recently used apps
+defaults write com.apple.dock "show-recents" -bool "false"
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Brave.app"
+dockutil --no-restart --add "/Applications/Calendar.app"
+
+killall Dock
+echo "Dock is set."
+
+### FINDER ###
+
+# Show the pathbar in the bottom of the Finder Windows
+defaults write com.apple.finder "ShowPathbar" -bool "true"
+
+# Set the default view style for folders without custom setting
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+
+# Do not display the warning when changing a file extension in Finder
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
+
+# Keep folders on top when sorting
+defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
+
+killall Finder
+echo "Finder is set."
+
+
