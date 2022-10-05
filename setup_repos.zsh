1e804cff5ee98b753c13d1c8c7897ed7bb5ff57e
alblesniak <alblesniak@users.noreply.github.com>
2022-10-05 11:05:00 +0200

    tmp moved to inside directory in plugins

--- setup_repos.zsh
+++ setup_repos.zsh
@@ -4,4 +4,4 @@
 echo '\n<<< Starting Cloning Repositories from Github >>>\n'
 
 # TPM - Tmux Plugin Manager
-git clone https://github.com/tmux-plugins/tpm config/tmux/plugins/
+git clone https://github.com/tmux-plugins/tpm config/tmux/plugins/tpm
