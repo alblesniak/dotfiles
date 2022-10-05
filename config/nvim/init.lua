e5038173f83ee8ac43ba2ffa80e2c411c606f8e4
alblesniak <alblesniak@users.noreply.github.com>
2022-10-05 17:32:34 +0200

    new neovim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,12 +1,21 @@
-require('packer-config')
-require('colorscheme-config.tokyonight')
-require('options')
-require('keymaps')
-require('nvim-tree-config')
-require('lsp-config.language-servers')
-require('lsp-config.nvim-cmp')
-require('lsp-config.mason')
-require('lsp-config.lsp-lines')
-require('lsp_lines').setup()
-require('lualine-config')
-require('treesitter-config')
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.gitsigns"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.lualine"
+require "user.toggleterm"
+require "user.project"
+require "user.impatient"
+require "user.indentline"
+require "user.alpha"
+require "user.whichkey"
+require "user.autocommands"
